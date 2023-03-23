0d3284c5fba8c51818e0da1b19659eb5dc1d2257
Lanre Adeolu <ladeolu2016@gmail.com>
2023-03-23 14:37:52 +0100

    added colors to the logo and created carousel for the mobile

--- src/pages/products/Products.js
+++ src/pages/products/Products.js
@@ -8,10 +8,12 @@ import img5 from "../../images/adao-14.png";
 import img6 from "../../images/adao-17.png";
 import img7 from "../../images/adao-18.png";
 import img8 from "../../images/adao-20.png";
+import img9 from "../../images/adao-5.png";
 
 const Products = () => {
   return (
     <>
+      {/* Desktop carousel */}
       <section className="d-none d-lg-block">
         <div id="carouselExampleIndicators" class="carousel slide">
           <div className="carou">
@@ -55,7 +57,7 @@ const Products = () => {
                     <div className="col-lg-3 col-md-3 col-10">
                       <div class="card" style={{ width: "15rem" }}>
                         <div className="card-image">
-                          <img src={img2} class="card-img-top" alt="..." />
+                          <img src={img9} class="card-img-top" alt="..." />
                         </div>
                         <div class="card-body">
                           <h5 class="card-title">Card title</h5>
@@ -114,24 +116,6 @@ const Products = () => {
                     <div className="col-lg-3 col-md-4 col-10">
                       <div class="card" style={{ width: "15rem" }}>
                         <div className="card-image">
-                          <img src={img5} class="card-img-top" alt="..." />
-                        </div>
-                        <div class="card-body">
-                          <h5 class="card-title">Card title</h5>
-                          <p class="card-text">
-                            Some quick example text to build on the card title
-                            and make up the bulk of the card's content.
-                          </p>
-                          <a href="#" class="btn btn-primary">
-                            Go somewhere
-                          </a>
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className="col-lg-3 col-md-3 col-10">
-                      <div class="card" style={{ width: "15rem" }}>
-                        <div className="card-image">
                           <img src={img6} class="card-img-top" alt="..." />
                         </div>
                         <div class="card-body">
@@ -151,6 +135,24 @@ const Products = () => {
                       <div class="card" style={{ width: "15rem" }}>
                         <div className="card-image">
                           <img src={img7} class="card-img-top" alt="..." />
+                        </div>
+                        <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">
+                            Some quick example text to build on the card title
+                            and make up the bulk of the card's content.
+                          </p>
+                          <a href="#" class="btn btn-primary">
+                            Go somewhere
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="col-lg-3 col-md-3 col-10">
+                      <div class="card" style={{ width: "15rem" }}>
+                        <div className="card-image">
+                          <img src={img5} class="card-img-top" alt="..." />
                         </div>
                         <div class="card-body">
                           <h5 class="card-title">Card title</h5>
@@ -261,6 +263,91 @@ const Products = () => {
                 // class="carousel-control-next"
                 type="button"
                 data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="next"
+              >
+                <span class="btn-prev-next" aria-hidden="true">
+                  <i class="bi bi-chevron-right"></i>
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Mobile carousel */}
+      <section className="carou-mobile d-block d-lg-none">
+        <div id="carouselExample" class="carousel slide">
+          <div className="carou">
+            <div>
+              <button
+                type="button"
+                data-bs-target="#carouselExample"
+                data-bs-slide="prev"
+              >
+                <span class="btn-prev-next" aria-hidden="true">
+                  {" "}
+                  <i class="bi bi-chevron-left"></i>
+                </span>
+              </button>
+            </div>
+
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <div class="card">
+                  <div className="card-image">
+                    <img src={img3} class="card-img-top" alt="..." />
+                  </div>
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                    <a href="#" class="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <div class="card">
+                  <div className="card-image">
+                    <img src={img1} class="card-img-top" alt="..." />
+                  </div>
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                    <a href="#" class="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <div class="card">
+                  <div className="card-image">
+                    <img src={img4} class="card-img-top" alt="..." />
+                  </div>
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                    <a href="#" class="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <button
+                type="button"
+                data-bs-target="#carouselExample"
                 data-bs-slide="next"
               >
                 <span class="btn-prev-next" aria-hidden="true">
