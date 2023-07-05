430228f3e4f00c829439add3a0ad4f253ddb264d
Lanre Adeolu <ladeolu2016@gmail.com>
2023-07-05 16:23:21 +0100

    edited exhibitions for mobile

--- src/pages/exhibitions/Exhibitions.js
+++ src/pages/exhibitions/Exhibitions.js
@@ -19,12 +19,12 @@ import img16 from "../../images/adao-11.png";
 const Exhibitions = () => {
   return (
     <>
-      <main id="main-exhibitions">
+      <main id="main-exhibitions" className="d-none d-md-block">
         <div className="container" id="exhibitions">
           <h3 className="section-heading">Exhibitions</h3>
         </div>
 
-        <section className="d-none d-md-block gallery-section">
+        <section className=" gallery-section">
           <div id="carouselExampleCaptions" className="carousel slide">
             <div className="gallery-buttons">
               <button
@@ -191,30 +191,46 @@ const Exhibitions = () => {
       </main>
 
       {/* Mobile */}
-      <div className="d-block d-md-none">
-        <div id="carouselExampleCaptions" className="carousel slide">
+      <main className="d-block d-md-none" id="main-exhibitions">
+        <div className="container" id="exhibitions">
+          <h3 className="section-heading">Exhibitions</h3>
+        </div>
+        <div
+          id="carouselExampleTest"
+          className="carousel slide mobile-carousel"
+          data-bs-ride="carousel"
+        >
           <div className="carousel-inner">
-            <div className="carousel-item active">
+            <div
+              className="carousel-item active slide-container"
+              data-bs-interval="4000"
+            >
               <div className="gallery-image">
-                <img src={img9} alt="..." />
+                <img src={img9} alt="exhibition-1" />
               </div>
               <div className="carousel-caption">
                 <h5>ADAO - Exhibition A</h5>
                 <p>Lagos, Nigeria</p>
               </div>
             </div>
-            <div className="carousel-item">
+            <div
+              className="carousel-item slide-container"
+              data-bs-interval="3000"
+            >
               <div className="gallery-image">
-                <img src={img9} alt="..." />
+                <img src={img5} alt="exhibition-2" />
               </div>
               <div className="carousel-caption">
                 <h5>ADAO - Exhibition B</h5>
                 <p>Texas, United States</p>
               </div>
             </div>
-            <div className="carousel-item">
+            <div
+              className="carousel-item slide-container"
+              data-bs-interval="3000"
+            >
               <div className="gallery-image">
-                <img src={img9} alt="..." />
+                <img src={img12} alt="exhibition-3" />
               </div>
               <div className="carousel-caption">
                 <h5>ADAO - Exhibition C</h5>
@@ -225,17 +241,18 @@ const Exhibitions = () => {
           <button
             class="carousel-control-prev"
             type="button"
-            data-bs-target="#carouselExample"
+            data-bs-target="#carouselExampleTest"
             data-bs-slide="prev"
           >
             <span class="btn-prev-next" aria-hidden="true">
               <i class="bi bi-chevron-left"></i>
             </span>
           </button>
+
           <button
             class="carousel-control-next"
             type="button"
-            data-bs-target="#carouselExample"
+            data-bs-target="#carouselExampleTest"
             data-bs-slide="next"
           >
             <span class="btn-prev-next" aria-hidden="true">
@@ -243,7 +260,7 @@ const Exhibitions = () => {
             </span>
           </button>
         </div>
-      </div>
+      </main>
     </>
   );
 };
