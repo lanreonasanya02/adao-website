7a0bb9e2a5093d8e24de9110a74483418bd353b1
Lanre Adeolu <ladeolu2016@gmail.com>
2023-07-29 23:27:44 +0100

    Media queries: worked home page for justifying text in overview and experts

--- src/pages/experts/Experts.js
+++ src/pages/experts/Experts.js
@@ -19,17 +19,16 @@ const Experts = () => {
         </div>
 
         <section className="container-lg">
-          <p>
-            Our services include a number of crucial areas to guarantee a smooth
-            art supply process for branded hotels: art curation, art
-            procurement, custom artwork, art installation, art maintenance and
-            conservation, and art engagement programs.
-          </p>
-
-          <p>
-            Have a question, need to know more information about our products,
-            company or delivery? Contact us and ask anything.
-          </p>
+          <div className="expert-paragraph">
+            <p>
+              Our services include a number of crucial areas to guarantee a
+              smooth art supply process for branded hotels: art curation, art
+              procurement, custom artwork, art installation, art maintenance and
+              conservation, and art engagement programs. Have a question, need
+              to know more information about our products, company or delivery?
+              Contact us and ask anything.
+            </p>
+          </div>
 
           <div className="expert-btn-container">
             <Link to="#" onClick={openForm} class="btn expert-btn">
--- src/pages/products/ProductCards.js
+++ src/pages/products/ProductCards.js
@@ -32,26 +32,29 @@ function Painting() {
     <>
       <h3 className="productCards-heading">Paintings & Drawings</h3>
 
-      <p>
-        Welcome to our Paintings and Drawings section, a captivating gallery
-        that unveils a tapestry of artistic brilliance! Here, we invite you to
-        embark on a visual odyssey through a mesmerizing array of art paintings
-        that embody the essence of creativity and expression and the raw emotion
-        captured within the lines of exquisite drawings. Our gallery unveils an
-        eclectic mix of styles, genres, and themes, each a testament to the
-        unique visions of our talented artists. Each stroke of the brush, every
-        hue carefully chosen, and the emotions etched upon the canvas, together
-        form a symphony of captivating narratives.
-      </p>
+      <div className="productCards-para-container">
+        <p>
+          Welcome to our Paintings and Drawings section, a captivating gallery
+          that unveils a tapestry of artistic brilliance! Here, we invite you to
+          embark on a visual odyssey through a mesmerizing array of art
+          paintings that embody the essence of creativity and expression and the
+          raw emotion captured within the lines of exquisite drawings. Our
+          gallery unveils an eclectic mix of styles, genres, and themes, each a
+          testament to the unique visions of our talented artists. Each stroke
+          of the brush, every hue carefully chosen, and the emotions etched upon
+          the canvas, together form a symphony of captivating narratives.
+        </p>
 
-      <p className="productCards-paragraph">
-        Whether you're a seasoned art connoisseur or a curious explorer, we
-        invite you to immerse yourself in the magic of visual art, where
-        imagination knows no bounds. Be inspired, be moved, and be captivated by
-        the evocative narratives woven into each brushstroke and pencil mark.
-        Experience the transformative power of art as you journey through our
-        gallery of paintings and drawings and let your imagination take flight!
-      </p>
+        <p className="productCards-paragraph">
+          Whether you're a seasoned art connoisseur or a curious explorer, we
+          invite you to immerse yourself in the magic of visual art, where
+          imagination knows no bounds. Be inspired, be moved, and be captivated
+          by the evocative narratives woven into each brushstroke and pencil
+          mark. Experience the transformative power of art as you journey
+          through our gallery of paintings and drawings and let your imagination
+          take flight!
+        </p>
+      </div>
 
       <div className="card-container">
         {filteredProducts.map((product) => {
@@ -147,23 +150,26 @@ function Sculpture() {
   return (
     <>
       <h3 className="productCards-heading">Sculptures</h3>
-      <p>
-        Discover our Sculptures section, a realm of three-dimensional artistry!
-        Experience stunning sculptures, each a masterpiece of creativity and
-        craftsmanship. From stone to metal, wood to clay, immerse in textures
-        and captivating narratives. Explore contemporary abstracts to timeless
-        figurative pieces. Indulge your senses, touch art, and embrace the
-        transformative power of sculptural expression.
-      </p>
 
-      <p className="productCards-paragraph">
-        Explore the captivating world of sculptures, where art transcends the
-        boundaries of the two-dimensional and invites you to touch the very soul
-        of creativity. Embrace the tactile allure and immerse yourself in the
-        transformative power of sculptural expression. Enjoy the journey through
-        our gallery of sculptures, where each creation stands as a testament to
-        the beauty of art in its tangible form.
-      </p>
+      <div className="productCards-para-container">
+        <p>
+          Discover our Sculptures section, a realm of three-dimensional
+          artistry! Experience stunning sculptures, each a masterpiece of
+          creativity and craftsmanship. From stone to metal, wood to clay,
+          immerse in textures and captivating narratives. Explore contemporary
+          abstracts to timeless figurative pieces. Indulge your senses, touch
+          art, and embrace the transformative power of sculptural expression.
+        </p>
+
+        <p className="productCards-paragraph">
+          Explore the captivating world of sculptures, where art transcends the
+          boundaries of the two-dimensional and invites you to touch the very
+          soul of creativity. Embrace the tactile allure and immerse yourself in
+          the transformative power of sculptural expression. Enjoy the journey
+          through our gallery of sculptures, where each creation stands as a
+          testament to the beauty of art in its tangible form.
+        </p>
+      </div>
 
       <div className="card-container">
         {filteredProducts.map((product) => {
@@ -260,19 +266,22 @@ function Ceramics() {
   return (
     <>
       <h3 className="productCards-heading">Ceramics</h3>
-      <p className="productCards-paragraph">
-        Welcome to our Ceramics section, a captivating showcase of exquisite
-        artistry and the timeless beauty of clay. Discover delicately
-        handcrafted vases, intricately sculpted figurines, and more. Immerse
-        yourself in the allure of unique glazes and forms. Whether you're an
-        avid collector or a curious enthusiast, we invite you to explore the
-        charm and allure of ceramics, where the artistic journey unfolds through
-        the hands of talented artisans. Embrace the tactile beauty, be inspired
-        by the interplay of colors and forms, and immerse yourself in the
-        timeless art of ceramics. Enjoy the journey through our gallery, where
-        each ceramic piece whispers the secrets of its creation and the passion
-        of its maker.
-      </p>
+
+      <div className="productCards-para-container">
+        <p className="productCards-paragraph">
+          Welcome to our Ceramics section, a captivating showcase of exquisite
+          artistry and the timeless beauty of clay. Discover delicately
+          handcrafted vases, intricately sculpted figurines, and more. Immerse
+          yourself in the allure of unique glazes and forms. Whether you're an
+          avid collector or a curious enthusiast, we invite you to explore the
+          charm and allure of ceramics, where the artistic journey unfolds
+          through the hands of talented artisans. Embrace the tactile beauty, be
+          inspired by the interplay of colors and forms, and immerse yourself in
+          the timeless art of ceramics. Enjoy the journey through our gallery,
+          where each ceramic piece whispers the secrets of its creation and the
+          passion of its maker.
+        </p>
+      </div>
 
       <div className="card-container">
         {filteredProducts.map((product) => {
@@ -364,18 +373,21 @@ function Photography() {
   return (
     <>
       <h3 className="productCards-heading">Photography</h3>
-      <p className="productCards-paragraph">
-        Welcome to our Photography section, a captivating visual journey through
-        breathtaking moments frozen in time. Explore a mesmerizing collection of
-        images, from intimate portraits to stunning landscapes, each capturing
-        the essence of the world around us. Immerse yourself in the artistry and
-        storytelling prowess of talented photographers, as they invite you to
-        see the world through their eyes. Whether you're a photography
-        enthusiast or simply appreciate visual storytelling, be inspired and
-        moved by the evocative narratives woven into each frame. Enjoy the
-        transformative power of photography as you explore our gallery of
-        captivating images.
-      </p>
+
+      <div className="productCards-para-container">
+        <p className="productCards-paragraph">
+          Welcome to our Photography section, a captivating visual journey
+          through breathtaking moments frozen in time. Explore a mesmerizing
+          collection of images, from intimate portraits to stunning landscapes,
+          each capturing the essence of the world around us. Immerse yourself in
+          the artistry and storytelling prowess of talented photographers, as
+          they invite you to see the world through their eyes. Whether you're a
+          photography enthusiast or simply appreciate visual storytelling, be
+          inspired and moved by the evocative narratives woven into each frame.
+          Enjoy the transformative power of photography as you explore our
+          gallery of captivating images.
+        </p>
+      </div>
 
       <div className="card-container">
         {filteredProducts.map((product) => {
